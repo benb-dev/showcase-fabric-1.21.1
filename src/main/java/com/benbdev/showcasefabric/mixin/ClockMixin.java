ccbf2ea9de89ce875548f8f3169ebd0e8a4b8c01
benb-dev <benjamin.david0918@gmail.com>
2025-10-12 23:58:35 -0400

    Used a Mixin to modify the Clock item's behavior to send the advance_time packet.

--- src/main/java/com/benbdev/showcasefabric/mixin/ClockMixin.java
+++ src/main/java/com/benbdev/showcasefabric/mixin/ClockMixin.java
@@ -1,12 +1,12 @@
 package com.benbdev.showcasefabric.mixin;
 
+import com.benbdev.showcasefabric.AdvanceTimeC2SPayload;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,16 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Item.class)
 public class ClockMixin {
     @Inject(method = "use", at = @At("HEAD"))
-    private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        ItemStack stack = user.getStackInHand(hand);
-        if (!stack.isOf(Items.CLOCK)) return;
-
-        if (world.isClient) {
-            // Client sends packet to server to request the time change
-            //ModNetworking.sendAdvanceTimePacket();
+    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        if (world.isClient()) {
+            AdvanceTimeC2SPayload payload = new AdvanceTimeC2SPayload(user.getBlockPos());
+            ClientPlayNetworking.send(payload);
         }
-
-        user.swingHand(hand);
-        cir.setReturnValue(ActionResult.success(true));
     }
 }
