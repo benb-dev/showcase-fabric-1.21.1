192672cdee88003eaf1a1a04c260e279eaa8aba4
benb-dev <benjamin.david0918@gmail.com>
2025-10-13 13:45:15 -0400

    Changed Lightning Staff to use raycast.

--- src/main/java/com/benbdev/showcasefabric/item/LightningStaffItem.java
+++ src/main/java/com/benbdev/showcasefabric/item/LightningStaffItem.java
@@ -10,8 +10,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class LightningStaffItem extends Item {
@@ -21,19 +26,43 @@ public class LightningStaffItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+
         if (world.isClient()) {
-            return TypedActionResult.pass(user.getStackInHand(hand));
+            // Perform a raycast up to 100 blocks
+            HitResult hit = user.raycast(100.0, 0.0F, false);
+            BlockPos targetPos = null;
+
+            if (hit.getType() == HitResult.Type.BLOCK) {
+                targetPos = ((BlockHitResult) hit).getBlockPos();
+            } else if (hit.getType() == HitResult.Type.MISS) {
+                Vec3d end = user.getCameraPosVec(0).add(user.getRotationVec(0).multiply(100.0));
+                targetPos = BlockPos.ofFloored(end);
+            }
+
+            if (targetPos != null) {
+                SummonLightningC2SPayload payload = new SummonLightningC2SPayload(targetPos);
+                ClientPlayNetworking.send(payload);
+            }
+
+            return TypedActionResult.success(user.getStackInHand(hand));
         }
 
+        return TypedActionResult.pass(user.getStackInHand(hand));
 
-        SummonLightningC2SPayload payload = new SummonLightningC2SPayload(user.getBlockPos());
-        ClientPlayNetworking.send(payload);
-//        SummonLightningS2CPayload payload = new SummonLightningS2CPayload(user.getBlockPos());
-//
-//        for (ServerPlayerEntity player : PlayerLookup.world((ServerWorld) world)) {
-//            ServerPlayNetworking.send(player, payload);
+
+//        if (world.isClient()) {
+//            return TypedActionResult.pass(user.getStackInHand(hand));
 //        }
-
-        return TypedActionResult.success(user.getStackInHand(hand));
+//
+//
+//        SummonLightningC2SPayload payload = new SummonLightningC2SPayload(user.getBlockPos());
+//        ClientPlayNetworking.send(payload);
+////        SummonLightningS2CPayload payload = new SummonLightningS2CPayload(user.getBlockPos());
+////
+////        for (ServerPlayerEntity player : PlayerLookup.world((ServerWorld) world)) {
+////            ServerPlayNetworking.send(player, payload);
+////        }
+//
+//        return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
