f4e00c8783d89235bf1d8886e103cf6635793c61
benb-dev <benjamin.david0918@gmail.com>
2025-10-14 20:57:32 -0400

    Fixed advance_time bug.

--- src/main/java/com/benbdev/showcasefabric/mixin/ClockMixin.java
+++ src/main/java/com/benbdev/showcasefabric/mixin/ClockMixin.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -18,8 +19,10 @@ public class ClockMixin {
     @Inject(method = "use", at = @At("HEAD"))
     public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
         if (world.isClient()) {
-            AdvanceTimeC2SPayload payload = new AdvanceTimeC2SPayload(user.getBlockPos());
-            ClientPlayNetworking.send(payload);
+            if (user.isHolding(Items.CLOCK)) {
+                AdvanceTimeC2SPayload payload = new AdvanceTimeC2SPayload(user.getBlockPos());
+                ClientPlayNetworking.send(payload);
+            }
         }
     }
 }
